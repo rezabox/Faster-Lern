89eb04f6d9019915e1fb7602dc3302164dbeb1b2
rezabox <reza.asareh81@gmail.com>
2024-11-15 15:06:39 +0330

    create slider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header';
 import '../styles/globals.css';
 
 export const metadata = {
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
    return (
       <html lang="en">
          <body>
+            <Header/>
             {children}
          </body>
       </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import Header from "@/components/Header";
+import Slider from "@/components/mainSlider";
 import  { AiFillCopyrightCircle } from "react-icons/ai";
 
 const Home = () => {
    return (
       <main className="">
-         <Header/>
+         <Slider/>
          {/* <div className=" bg-zinc-100 rounded-md flex flex-col  gap-8  p-8">
             <div>HELLOW, THIS IS MERNFA</div>
             <div>سلام من مرن فا هستم...</div>
