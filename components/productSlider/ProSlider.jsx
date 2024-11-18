00360204370234ae420d6303e8248e35f21f41f1
rezabox <reza.asareh81@gmail.com>
2024-11-18 09:38:15 +0330

    updated proSlider

--- components/productSlider/ProSlider.jsx
+++ components/productSlider/ProSlider.jsx
@@ -5,7 +5,7 @@ import { FaChevronLeft } from "react-icons/fa";
 import SliderBox1 from "./SliderBox";
 import Link from "next/link";
 
-const ProSlider1 = () => {
+const ProSlider1 = ({title, linkComp}) => {
   const carouselRef = useRef();
   const caroselSwitcher = (data) => {
     if (carouselRef.current) {
@@ -18,12 +18,12 @@ const ProSlider1 = () => {
   };
 
   return (
-    <main className="bg-indigo-500 mb-40 mt-5 p-5">
+    <main className="bg-indigo-500 mb-[30px] mt-5 p-5">
       <div className="container mx-auto py-8">
         <section className="flex flex-col gap-4">
           <header className="flex justify-between items-center ">
             <h2 className="font-semibold text-2xl border-r-white pr-1 text-white">
-              اپلیکیشن ها
+              {title}
             </h2>
             <div className="flex items-center justify-between gap-5">
               <div className="flex items-center gap-1 text-zinc-500">
@@ -36,7 +36,7 @@ const ProSlider1 = () => {
                   className="cursor-pointer bg-zinc-200 transition-all duration-300 hover:bg-orange-400 w-10 h-10 p-3 rounded"
                 />
               </div>
-              <Link href={'/'} className="bg-orange-500 border-white border-2 px-4 py-2 rounded-md transition-all duration-500 text-white hover:bg-orange-400">مشاهده همه </Link>
+              <Link href={linkComp} className="bg-orange-500 border-white border-2 px-4 py-2 rounded-md transition-all duration-500 text-white hover:bg-orange-400">مشاهده همه </Link>
             </div>
           </header>
           <div
