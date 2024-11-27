55d0b57741c3e5a99e0e88d39664823f61b13344
rezabox <reza.asareh81@gmail.com>
2024-11-27 23:59:14 +0330

    add account,cart,contact,dashboard, help

---
+++ app/contact/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () =>{
+  return (
+    <section className='container mx-auto p-12 flex items-center justify-center'>
+        <div className="px-8 py-4 rounded-md bg-orange-500 text-white ">تماس با ما</div>
+    </section>
+  )
+}
+
+export default page;
---
+++ app/help/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () =>{
+  return (
+    <section className='container mx-auto p-12 flex items-center justify-center gap-4 flex-wrap'>
+        <div className="px-8 py-4 rounded-md bg-orange-500 text-white ">قوانین سایت</div>
+        <div className="px-8 py-4 rounded-md bg-orange-500 text-white ">حریم خصوصی </div>
+        <div className="px-8 py-4 rounded-md bg-orange-500 text-white ">سوالات متداول</div>
+        <div className="px-8 py-4 rounded-md bg-orange-500 text-white ">چگونه خرید کنیم ؟</div>
+    </section>
+  )
+}
+
+export default page;
