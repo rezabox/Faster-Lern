6ab71018ee42ce20aa595ff48cbd425b2971681e
rezabox <reza.asareh81@gmail.com>
2024-12-09 01:30:32 +0330

    bugfixed allMidelBanner

--- components/dashboardCtrl/forms/allMidBanners/index.jsx
+++ components/dashboardCtrl/forms/allMidBanners/index.jsx
@@ -6,14 +6,28 @@ import Image from "next/image";
 
 const AllMidBanners = () => {
   const [banners, setbanners] = useState([]);
-  const [pageNumber, setPageNumber] = useState(1);
 
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numbersOfbtns, setnumberOfBtns] = useState([-1]);
+
+  const goTopCtrl = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   useEffect(() => {
     axios
       .get(`http://localhost:27017/api/middle-banners?pn=${pageNumber}`)
-      .then((d) => setbanners(d.data))
+      .then((d) => {
+        setbanners(d.data.GoalMidBans);
+        setnumberOfBtns(
+          Array.from(Array(Math.ceil(d.data.AllMidBansNum / 2)).keys())
+        );
+        goTopCtrl();
+      })
       .catch((e) => console.log(e.data));
-  }, []);
+  }, [pageNumber]);
 
   return (
     <div className="p-4 flex flex-col gap-8">
@@ -31,7 +45,30 @@ const AllMidBanners = () => {
           banners.map((ba, i) => <Box key={i} data={ba} />)
         )}
       </div>
-      <div className="">buttons</div>
+      <div className="flex justify-center gap-4 items-center">
+        {numbersOfbtns[0] == -1 ? (
+          <div className="flex justify-center items-center p-12">
+            <Image
+              alt="loading"
+              width={40}
+              height={40}
+              src={"/loading.svg"}
+            />
+          </div>
+        ) : (
+          numbersOfbtns.map((da, i) => (
+            <button
+              className="flex items-center justify-center rounded-md bg-indigo-500 hover:bg-orange-400 transition-all duration-400 text-white w-12 h-12"
+              onClick={() => {
+                setPageNumber(da + 1);
+                setbanners([]);
+              }}
+            >
+              {da + 1}
+            </button>
+          ))
+        )}
+      </div>
     </div>
   );
 };
