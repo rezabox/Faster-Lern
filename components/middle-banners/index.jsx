4cd2a52d396d96f812187e55c031ac359ec002e8
rezabox <reza.asareh81@gmail.com>
2024-12-15 14:30:36 +0330

    updated middle-banner

--- components/middle-banners/index.jsx
+++ components/middle-banners/index.jsx
@@ -1,28 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
+const getData = async () => {
+   const data = await fetch(
+      "http://localhost:27017/api/get-active-mid-bans",
+      { cache: "no-store" }
+   );
+   return data.json();
+};
 
-const MiddleBanner = () => {
-  return (
-    <section className="container mx-auto md:flex md:justify-between p-5 md:p-0 justify-center items-center">
-      <Image
-        alt="alt"
-        className="rounded-xl"
-        width={600}
-        height={200}
-        src={"/images/banner-insta.jpg"}
-        objectFit="cover"
-        layout="fixed"
-      />
-      <Image
-        alt="alt"
-        className="rounded-xl mt-5 md:mt-0"
-        width={600}
-        height={200}
-        src={"/images/banner-insta.jpg"}
-        objectFit="cover"
-        layout="fixed"
-      />
-    </section>
-  );
+const MiddleBanner = async () => {
+   const data = await getData();
+
+   return (
+      <>
+         {
+         data.length < 1 ? (
+            <div></div>
+         ) : (
+            <section className=" container mx-auto flex justify-between items-center flex-wrap">
+               {data.map((banner, i) => (
+                  <Link className=" my-4" href={banner.link} key={i}>
+                     <Image
+                        className=" rounded-xl"
+                        alt={banner.imageAlt}
+                        title={banner.imageAlt}
+                        width={600}
+                        height={200}
+                        src={banner.image}
+                     />
+                  </Link>
+               ))}
+            </section>
+         )}
+      </>
+   );
 };
 
 export default MiddleBanner;
