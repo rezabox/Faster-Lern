f5a5148d132d90b095e608d4682d6d092f07fb07
rezabox <reza.asareh81@gmail.com>
2024-12-15 14:34:33 +0330

    updated form

--- components/dashboardCtrl/forms/index.jsx
+++ components/dashboardCtrl/forms/index.jsx
@@ -6,20 +6,21 @@ import NewBannerDetails from './MidBannerDerails';
 
 const MiddleBannerAll = () => {
   const [midBanDetCtrl,setmidBanCtrl] = useState("");
-  const [det,setDet] =useState(<AllMidBanners setmidBanCtrl={setmidBanCtrl}/>);
+  const [randNumForBannerClick, setrandNumForBannerClick] = useState(1);
+  const [det,setDet] =useState(<AllMidBanners setrandNumForBannerClick={setrandNumForBannerClick} setmidBanCtrl={setmidBanCtrl}/>);
   
   useEffect(()=>{
      if(midBanDetCtrl != "") {
        setDet(<NewBannerDetails midBanId={midBanDetCtrl}/>);
      }
-  },[midBanDetCtrl])
+  },[randNumForBannerClick])
 
   return (
     <div className='flex flex-col gap-8'>
         <section className='flex justify-between items-center gap-2'>
             <h1 className='txet-blue-500 text-lg'>بنر های تبلیغاتی </h1>
             <div className='flex justify-end items-center gap-2'>
-                <button onClick={() => setDet(<AllMidBanners setmidBanCtrl={setmidBanCtrl}/>)} className='px-3 py-1 rounded-md bg-indigo-600 text-white transition-all duration-500 hover:bg-orange-500'>همه</button>
+                <button onClick={() => setDet(<AllMidBanners setrandNumForBannerClick={setrandNumForBannerClick} setmidBanCtrl={setmidBanCtrl}/>)} className='px-3 py-1 rounded-md bg-indigo-600 text-white transition-all duration-500 hover:bg-orange-500'>همه</button>
                 <button onClick={() => setDet(<NewMidBaner/>)} className='px-3 py-1 rounded-md bg-indigo-600 text-white transition-all duration-500 hover:bg-orange-500'>بنر جدید</button>
             </div>
         </section>
@@ -31,5 +32,4 @@ const MiddleBannerAll = () => {
     </div>
   )
 }
-
 export default MiddleBannerAll;
